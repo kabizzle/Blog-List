940ebc891f113306b643c08b679a96f0830a7555
kabizzle <kabirrbissessar@gmail.com>
2024-03-31 18:59:26 +0300

    completed #1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,30 +34,6 @@ const App = () => {
       }, 5000)
     }
   }
-
-  const loginForm = () => {
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input 
-          type="text" 
-          value={username} 
-          name="Username" 
-          onChange={({ target }) => setUsername(target.value)}
-        />          
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />  
-      </div>
-      <button type="submit">login</button>
-    </form>
-  }
   
   if (user===null) {
     return (
